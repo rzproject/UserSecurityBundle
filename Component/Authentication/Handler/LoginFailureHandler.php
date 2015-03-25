a59e77f9623bb8801b0f297f3f0c2d3262add378
mellzamora <mellzamora@outlook.com>
2015-03-25 11:43:16 +0800

    updated user security

--- Component/Authentication/Handler/LoginFailureHandler.php
+++ Component/Authentication/Handler/LoginFailureHandler.php
@@ -2,18 +2,63 @@
 
 namespace  Rz\UserSecurityBundle\Component\Authentication\Handler;
 
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-use CCDNUser\SecurityBundle\Component\Authentication\Tracker\LoginFailureTracker;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use CCDNUser\SecurityBundle\Component\Authentication\Handler\LoginFailureHandler as BaseLoginFailureHandler;
+
 
 class LoginFailureHandler extends BaseLoginFailureHandler
 {
+
+    /**
+     *
+     * @access public
+     * @param  \Symfony\Component\HttpFoundation\Request                                                     $request
+     * @param  \Symfony\Component\Security\Core\Exception\AuthenticationException                            $exception
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        // Get the visitors IP address and attempted username.
+        $ipAddress = $request->getClientIp();
+        if ($request->request->has('_username')) {
+            $username = $request->request->get('_username');
+        } else {
+            $username = '';
+        }
+
+        // Make a note of the failed login.
+        $this->loginFailureTracker->addAttempt($ipAddress, $username);
+        $request->getSession()->set(SecurityContext::AUTHENTICATION_ERROR, $exception);
+        $request->getSession()->getFlashBag()->set(SecurityContext::AUTHENTICATION_ERROR, $exception->getMessage());
+
+        // Send response back to browser depending on wether this is XML request or not.
+        if ($request->isXmlHttpRequest() || $request->request->get('_format') === 'json') {
+            $response = new Response(
+                json_encode(
+                    array(
+                        'status' => 'failed',
+                        'errors' => array($exception->getMessage())
+                    )
+                )
+            );
+
+            $response->headers->set('Content-Type', 'application/json');
+        } else {
+            $response = new RedirectResponse(
+                $this->router->generate(
+                    $this->routeLogin['name'],
+                    $this->routeLogin['params']
+                )
+            );
+        }
+
+        return $response;
+    }
 
 }
